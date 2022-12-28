ed47daf6fdce64b35fa51b48e5d5e9e02aed3014
Rafeif <114472406+Rafeif@users.noreply.github.com>
2022-12-28 19:37:03 +0200

    $ git commit -m "pro"

---
+++ src/Components/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import NavBar from './Navbar/NavBar';
+import Home from './Home/Home';
+import About from './About/About';
+import Login from './Login/Login';
+import People from './People/People'
+import Register from './Register/Register';
+import TvShows from './TvShows/TvShows';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from './NotFound/NotFound';
+import Movies from './Movies/Movies';
+const App = () => {
+  return (
+    <div>
+      <NavBar/>
+      <Routes>
+      <Route path='/' element={<Home/>}></Route>
+        <Route path='home' element={<Home/>}></Route>
+        <Route path='about' element={<About/>}></Route>
+        <Route path='movies' element={<Movies/>}></Route>
+        <Route path='login' element={<Login/>}></Route>
+        <Route path='people' element={<People/>}></Route>
+        <Route path='register' element={<Register/>}></Route>
+        <Route path='tvshowes' element={<TvShows/>}></Route>
+        <Route path='*' element={<NotFound/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Register/Register.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { useState } from 'react';
+const Register = () => {
+    let[user,setUser]=useState({
+firstName:'',
+lastName:'',
+age:'',
+email:'',
+mYpassword:'',
+});
+let submitFormData=(e)=>{
+e.preventDefault();
+};
+let getFormValue=(e)=>{
+let myUser={...user};
+myUser[e.target.name]=e.target.value;
+setUser(myUser);
+console.log(myUser)
+       }
+    return (
+        <div>
+<form onSubmit={()=>submitFormData} className='container'>
+  <div className="mb-3 ">
+    <label htmlFor="firstName" className="form-label">First Name</label>
+    <input type="text" onChange={getFormValue}  name='firstName' className="form-control" id="firstName" aria-describedby="emailHelp" />
+  </div>
+  <div className="mb-3 ">
+    <label htmlFor="lastName" className="form-label">Last Name</label>
+    <input type="text" onChange={getFormValue} name ='lastName'className="form-control" id="lastName" aria-describedby="emailHelp" />
+  </div>
+  <div className="mb-3 ">
+    <label htmlFor="age" className="form-label">Age</label>
+    <input type="number" onChange={getFormValue} name ='age'className="form-control" id="age" aria-describedby="emailHelp" />
+  </div>
+  <div className="mb-3 ">
+    <label htmlFor="email" className="form-label">Email address</label>
+    <input type="email" onChange={getFormValue} name ='email' className="form-control" id="email" aria-describedby="emailHelp" />
+    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="mYpassword" className="form-label">Password</label>
+    <input type="password" onChange={getFormValue} name='mYpassword' className="form-control" id="mYpassword" />
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+
+        </div>
+    );
+}
+
+export default Register;
